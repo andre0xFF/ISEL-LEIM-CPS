564322d04d132e76bf559a3ad4f161fbf7787abc
Andre Fonseca <afbfonseca@gmail.com>
2017-10-23 15:52:15 +0100

    Add barcode generator example from class

---
+++ Class/codigo_detector_erros_livros.py
@@ -0,0 +1,23 @@
+import numpy as np
+np.array([0,3,8,7,9,4,6,6,5])
+array([0, 3, 8, 7, 9, 4, 6, 6, 5])
+np.array([0,3,8,7,9,4,6,6,5])
+array([0, 3, 8, 7, 9, 4, 6, 6, 5])
+x = np.array([0,3,8,7,9,4,6,6,5])
+np.arange(10,0,-1))
+np.arange(10,0,-1)
+array([10,  9,  8,  7,  6,  5,  4,  3,  2,  1])
+np.arange(10,1,-1)
+array([10,  9,  8,  7,  6,  5,  4,  3,  2])
+np.arange(10,1,-1) * x
+array([ 0, 27, 64, 49, 54, 20, 24, 18, 10])
+np.sum(np.arange(10,1,-1) * x)
+266
+np.sum(np.arange(10,1,-1) * x) % 11
+2
+11 - np.sum(np.arange(10,1,-1) * x) % 11
+9
+# numero que precisamos de acrescentar
+... 
+
+
