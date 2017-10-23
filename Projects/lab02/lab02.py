c8d791cb456d53557249954aa521b57f7117008a
Andre Fonseca <afbfonseca@gmail.com>
2017-10-23 20:59:13 +0100

    Lab 02 initial commit

---
+++ Projects/lab02/lab02.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def main():
+    exercise_01()
+    exercise_02()
+    exercise_03()
+    exercise_04()
+    exercise_05()
+    exercise_06()
+
+
+def exercise_01():
+    pass
+
+
+def exercise_02():
+    pass
+
+
+def exercise_03():
+    pass
+
+
+def exercise_04():
+    pass
+
+
+def exercise_05():
+    pass
+
+
+def exercise_06():
+    pass
+
+
+if __name__ == '__main__':
+    main()
