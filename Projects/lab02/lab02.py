37e6b92c2947f620260f4e5394493681271a0c46
Andre Fonseca <afbfonseca@gmail.com>
2017-11-10 17:41:02 +0000

    Exercise 02 complete
    03 is in works

--- Projects/lab02/lab02.py
+++ Projects/lab02/lab02.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
-from lab01.lab01 import sawtooth_signal, quantify, uniform_midrise_quantizer
+from lab01.lab01 import sawtooth_signal, quantize, uniform_midrise_quantizer, quantization_interval, snr_theoric
 
 
 def main():
@@ -24,12 +24,12 @@ def example():
     n = np.arange(0, 8)
     m = np.round(np.sin(2 * np.pi * (np.float(1300) / 8000) * n), decimals=3)
 
-    mq, idx = quantify(m, vmax, vj, tj)
+    mq, idx = quantize(m, vmax, vj, tj)
 
     bin = pcm_encode(idx, 3)
     dec = pcm_decode(bin)
 
-    print('Quantize signal must be equal to (Encode > Decode > Dequantize): {}'. format(np.array_equal(mq, vj[dec])))
+    print('Quantized signal must be equal to (Encode > Decode > Dequantize): {}'. format(np.array_equal(mq, vj[dec])))
 
 
 def exercise_01():
@@ -37,9 +37,9 @@ def exercise_01():
     vmax = np.max(np.abs(signal))
     r = 3
 
-    delta_q = (2 * vmax) / (np.power(2, r))
+    delta_q = quantization_interval(vmax, r)
     vj, tj = uniform_midrise_quantizer(vmax, delta_q)
-    mq, idx = quantify(signal, vmax, vj, tj)
+    mq, idx = quantize(signal, vmax, vj, tj)
 
     bin = pcm_encode(idx, r)
     dec = pcm_decode(bin)
@@ -80,28 +80,45 @@ def gray_decode(bits):
 
 
 def exercise_02():
-    # TODO: SNR calculation
     fs, m = wav.read("lab01/som_8_16_mono.wav")
 
     vmax = np.max(np.abs(m))
     r = np.array([3, 5, 8])
+    snr = np.arange(len(r), dtype='float')
 
-    for i in r:
-        delta_q = (2 * vmax) / (np.power(2, i))
+    for i in range(len(r)):
+        delta_q = quantization_interval(vmax, r[i])
         vj, tj = uniform_midrise_quantizer(vmax, delta_q)
-        mq, idx = quantify(m, vmax, vj, tj)
+        mq, idx = quantize(m, vmax, vj, tj)
 
-        filename = 'lab02/som_8_16_quantize_{}.wav'.format(i)
+        filename = 'lab02/som_8_16_quantize_{}bits.wav'.format(r[i])
         wav.write(filename, fs, mq.astype('int16'))
 
-        bin = pcm_encode(idx, i)
+        bin = pcm_encode(idx, r[i])
         dec = pcm_decode(bin)
 
-        filename = 'lab02/som_8_16_quantize_encode_decode_dequantize_{}.wav'.format(i)
-        wav.write(filename, fs, vj[dec].astype('int16'))
+        filename = 'lab02/som_8_16_quantize_encode_decode_dequantize_{}bits.wav'.format(r[i])
+        wav.write(filename, fs, dequantize(vj, dec).astype('int16'))
+
+        p = np.sum(m * m) / len(m)
+        snr[i] = snr_theoric(r[i], p, vmax)
+
+
+def dequantize(vj, indexes):
+    return vj[indexes]
 
 
 def exercise_03():
+    n = 15
+    r = 11
+
+    m = sawtooth_signal()
+
+    vmax = np.max(np.abs(m))
+    delta_q = quantization_interval(vmax, r)
+    vj, tj = uniform_midrise_quantizer(vmax, delta_q)
+    mq, idx = quantize(m, vmax, vj, tj)
+
     # Parity matrix
     P = np.array([
         [1, 1, 1, 1],
@@ -117,11 +134,31 @@ def exercise_03():
         [1, 1, 0, 0],
     ])
 
-    G = np.hstack((np.identity(11), P))
+    # Encode and codify with Hamming(15, 11)
+    bin = pcm_encode(idx, r)
+    c = hamming(bin, P, n, r)
 
-    C = np.logical_xor(m, G)
-    # or
-    # C = np.dot(m, G) % 2
+    # Generate an error to simulate the channel
+    error = np.random.binomial(1, 0.01, len(c))
+    y = (c + error) % 2
+
+
+def hamming(message, P, n, r):
+    G = np.hstack((np.identity(15 - len(P[0])), P))
+
+    # C = np.logical_xor(m, G)
+    C = np.dot(message, G) % 2
+    return np.ndarray.flatten(C)
+
+
+def error_correction(message, P):
+    H = np.vstack((P, np.identity(len(P[0]))))
+    S = np.dot(message, H)
+
+    i = np.nonzero(S)[0]
+
+    if len(i) == 0:
+        return S
 
 
 def exercise_04():
