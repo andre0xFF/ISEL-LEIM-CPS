5cb47d95751e2a4375ea3e153ad9a522b9d9f4e6
Andre Fonseca <afbfonseca@gmail.com>
2017-11-13 21:01:16 +0000

    Lab03 initial commit

---
+++ Projects/lab03/lab03.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import lab02.lab02 as lab02
+
+
+def main():
+    exercise_01()
+    exercise_02()
+    exercise_03()
+    exercise_04()
+    exercise_05()
+    exercise_06()
+    exercise_07()
+
+
+def exercise_01():
+    pass
+
+
+def exercise_02():
+    pass
+
+
+def exercise_03():
+    pass
+
+
+def exercise_04():
+    pass
+
+
+def exercise_05():
+    pass
+
+
+def exercise_06():
+    pass
+
+
+def exercise_07():
+    pass
+
+
+if __name__ == '__main__':
+    main()
